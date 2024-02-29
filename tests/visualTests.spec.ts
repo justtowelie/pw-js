914908e7bf5bdc787f53d0de966352620b194e3a
Daniel Leggett <danielleggett1@gmail.com>
2024-02-29 10:47:50 +0000

    added visual comparison test

---
+++ tests/visualTests.spec.ts
@@ -0,0 +1,13 @@
+import { setUpPage } from '../utils/setUp';
+import { test } from '../lib/fixtures';
+
+test.beforeEach(async ({ page }) => {
+	await setUpPage(page);
+});
+
+test.describe('Visual comparison tests', async () => {
+	test('Successfully compare the screen of a predetermined product', async ({ commonMethods, purchases }) => {
+		await commonMethods.navigateTo('https://magento.softwaretestingboard.com/driven-backpack.html');
+		await commonMethods.waitForSelectorAndCompareScreenshot(purchases.price, 'product-page.png');
+	});
+});
